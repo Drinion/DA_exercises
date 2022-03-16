7064122c64a84423007a3b26680f9364c27f9bea
Johanna Studer <studer.johanna@gmail.com>
2022-03-16 16:28:41 +0100

    ex3.1 mit Comparator Implementierung

--- DA_ex03/NameVornameComparator.java
+++ DA_ex03/NameVornameComparator.java
@@ -7,31 +7,15 @@
  */
 
 public class NameVornameComparator implements java.util.Comparator<StudentIn> {
-  private String nameA;
-  private String nameB;
 
   public int compare(StudentIn a, StudentIn b) {
-    nameA = a.getName() + a.getVorname();
-    nameB = b.getName() + b.getVorname();
+    if (a.getName().compareTo(b.getName()) == 0) { // if same last Name
+      return a.getVorname().compareTo(b.getVorname()); // sort by Vorname
+    }
+    else{
+      return a.getName().compareTo(b.getName());// else sort by Name
+    }
 
-    return lexico(nameA, nameB);
   }
 
-  private static final int lexico(String nameA, String nameB) {
-    for(int i = 0; i < nameA.length() && i < nameB.length(); i++) {
-      if(nameA.charAt(i) == nameB.charAt(i)) {
-        continue;
-      }
-      else {
-        return (int)nameA.charAt(i) - (int)nameB.charAt(i);
-      }
-    }
-    if(nameA.length() < nameB.length()) {
-      return (nameB.length() - nameA.length());
-    }
-    else if(nameA.length() > nameB.length()) {
-      return (nameA.length() - nameB.length());
-    }
-    return 0;
-  }
 }
